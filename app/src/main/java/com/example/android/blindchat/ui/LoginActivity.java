5e5ccac5147ffd89c422b6fa4b525756b2b8f542
tianxinzhou <zhoutianxin@gmail.com>
2019-05-26 01:22:22 -0700

    Tianxin test

--- app/src/main/java/com/example/android/blindchat/ui/LoginActivity.java
+++ app/src/main/java/com/example/android/blindchat/ui/LoginActivity.java
@@ -1,17 +1,84 @@
 package com.example.android.blindchat.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.android.blindchat.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button loginBack;
+    TextInputEditText loginUsername;
+    TextInputEditText loginPassword;
+    TextView signupLink;
+    TextView forgetPasswordLink;
+    Button login;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        loginBack = (Button)findViewById(R.id.btn_login_back);
 
+        loginBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                backToInit();
+            }
+        });
+
+        loginUsername = (TextInputEditText)findViewById(R.id.login_username_input);
+        loginPassword = (TextInputEditText)findViewById(R.id.login_password_input);
+
+        signupLink = (TextView)findViewById(R.id.sign_up_link);
+        signupLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toSignup();
+            }
+        });
+
+        forgetPasswordLink = (TextView)findViewById(R.id.forget_password_link);
+        forgetPasswordLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toForget();
+            }
+        });
+
+        login = (Button)findViewById(R.id.btn_log_in);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toLogin();
+            }
+        });
+
+
+    }
+
+    private void backToInit() {
+        Intent intent = new Intent(this, InitActivity.class);
+        startActivity(intent);
+    }
+
+    private void toSignup() {
+        Intent intent = new Intent(this, SignupActivity.class);
+        startActivity(intent);
+    }
+    private void toForget() {
+        Intent intent = new Intent(this, PasswordResetActivity.class);
+        startActivity(intent);
+    }
+    private void toLogin() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/android/blindchat/ui/SignupActivity.java
+++ app/src/main/java/com/example/android/blindchat/ui/SignupActivity.java
@@ -1,6 +1,17 @@
 package com.example.android.blindchat.ui;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.android.blindchat.R;
+
 public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+
+    }
 }
