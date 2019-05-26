29db3bbc37feb7bfe49502e0b8f3d35dcc2ebb31
tianxinzhou <zhoutianxin@gmail.com>
2019-05-25 21:00:06 -0700

    Tianxin test

--- app/src/main/java/com/example/android/blindchat/ui/InitActivity.java
+++ app/src/main/java/com/example/android/blindchat/ui/InitActivity.java
@@ -1,6 +1,48 @@
 package com.example.android.blindchat.ui;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.android.blindchat.R;
 
 public class InitActivity extends AppCompatActivity {
+
+    Button logIn;
+    Button signUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_init);
+
+        logIn = (Button)findViewById(R.id.btn_log_in);
+        signUp = (Button)findViewById(R.id.btn_sign_up);
+
+        logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goLogIn();
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goSignUp();
+            }
+        });
+    }
+
+    public void goLogIn() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void goSignUp() {
+        Intent intent = new Intent(this, SignupActivity.class);
+        startActivity(intent);
+    }
 }
