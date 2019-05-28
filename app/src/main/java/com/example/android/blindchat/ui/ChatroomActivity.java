36913ff3b04daef9c2f906a5ed4a910462a89043
Celia Liang <celialiang0627@gmail.com>
2019-05-28 00:38:12 -0700

    chatroom firebase changes

--- app/src/main/java/com/example/android/blindchat/ui/ChatroomActivity.java
+++ app/src/main/java/com/example/android/blindchat/ui/ChatroomActivity.java
@@ -1,6 +1,120 @@
 package com.example.android.blindchat.ui;
 
+import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+
+import com.example.android.blindchat.R;
+import com.example.android.blindchat.adapter.MessageAdapter;
+import com.example.android.blindchat.model.Message;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChatroomActivity extends AppCompatActivity {
+
+    private String TAG = "ChatroomActivity";
+
+    public static final String ANONYMOUS = "anonymous";
+    public static final int DEFAULT_MSG_LENGTH_LIMIT = 1000;
+
+    private ListView mMessageListView;
+    private MessageAdapter mMessageAdapter;
+    private ImageButton mPhotoPickerButton;
+    private TextInputEditText mMessageEditText;
+    private Button mSendButton;
+
+    private String mUsername;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mMessageDatabaseReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mUsername = ANONYMOUS;
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        //TBD, db pasth: messages
+        mMessageDatabaseReference = mFirebaseDatabase.getReference().child("chatroom-messages");
+
+        // Initialize references to views
+        mMessageListView = (ListView) findViewById(R.id.chatroom_listview);
+        mMessageEditText = (TextInputEditText) findViewById(R.id.chatroom_textinput);
+        mSendButton = (Button) findViewById(R.id.chatroom_send_btn);
+
+        // Initialize message ListView and its adapter
+        List<Message> chatMessages = new ArrayList<>();
+        mMessageAdapter = new MessageAdapter(this, R.layout.item_message_view, chatMessages);
+        mMessageListView.setAdapter(mMessageAdapter);
+
+        // Initialize progress bar
+        //mProgressBar.setVisibility(ProgressBar.INVISIBLE);
+
+        // ImagePickerButton shows an image picker to upload a image for a message
+        mPhotoPickerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: Fire an intent to show an image picker
+            }
+        });
+
+        // Enable Send button when there's text to send
+        mMessageEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.toString().trim().length() > 0) {
+                    mSendButton.setEnabled(true);
+                } else {
+                    mSendButton.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
+        mMessageEditText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(DEFAULT_MSG_LENGTH_LIMIT)});
+
+        // Send button sends a message and clears the EditText
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: Send messages on click
+
+                // Clear input box
+                mMessageEditText.setText("");
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
 }
