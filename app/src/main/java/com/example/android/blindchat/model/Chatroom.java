d02ea919f37c15db4f608f9fdfc5c5e441318319
Yuxia Chen <chenyuxia90@gmail.com>
2019-05-28 22:10:48 -0700

    add create new chatroom functionality

--- app/src/main/java/com/example/android/blindchat/model/Chatroom.java
+++ app/src/main/java/com/example/android/blindchat/model/Chatroom.java
@@ -2,22 +2,99 @@ package com.example.android.blindchat.model;
 
 import java.io.Serializable;
 import java.sql.Time;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 
-public class ChatRoom implements Serializable {
+public class Chatroom implements Serializable {
     private String name;
     private String description;
     private User creator;
     private float longitude;
     private float latitude;
-    private Time created_at;
-    private ChatroomHistory chatroomHistory;
+    private String created_at;
+    private int member_number = 1;
+    private ArrayList<Message> chat_history;
+    private ArrayList<User> joined_users;
+    public Chatroom(){
 
-    private class ChatroomHistory {
-        ArrayList<ChatItem> items;
-        public class ChatItem {
-            private String username;
-            private String message;
-        }
     }
+
+    public Chatroom(String name, String description, String created_at){
+        this.name = name;
+        this.description = description;
+        this.created_at = created_at;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getMember_number() {
+        return member_number;
+    }
+
+    public void setMember_number(int member_number) {
+        this.member_number = member_number;
+    }
+
+    public User getCreator() {
+        return creator;
+    }
+
+    public void setCreator(User creator) {
+        this.creator = creator;
+    }
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+
+    public ArrayList<User> getJoined_users() {
+        return joined_users;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public void setJoined_users(ArrayList<User> joined_users) {
+        this.joined_users = joined_users;
+    }
+
+    public ArrayList<Message> getChat_history() {
+        return chat_history;
+    }
+
+    public void setChat_history(ArrayList<Message> chat_history) {
+        this.chat_history = chat_history;
+    }
+
 }
--- app/src/main/java/com/example/android/blindchat/ui/MainActivity.java
+++ app/src/main/java/com/example/android/blindchat/ui/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.android.blindchat.ui;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
