ff1e82e2091d031c70be96bf6130142db911df80
momo10229 <17simoli@gmail.com>
2019-06-03 14:26:23 -0700

    update pw reset, settings - change pw/name

--- app/src/main/java/com/example/android/blindchat/ui/ChangePasswordFragment.java
+++ app/src/main/java/com/example/android/blindchat/ui/ChangePasswordFragment.java
@@ -1,39 +1,125 @@
 package com.example.android.blindchat.ui;
 
-import android.media.Image;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.example.android.blindchat.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static android.support.constraint.Constraints.TAG;
 
 public class ChangePasswordFragment extends Fragment {
+
+    private ImageButton goback;
+    private EditText currentpw;
+    private EditText newpw;
+    private EditText newpwagain;
+    private Button savepw;
+    private String scurrentpw;
+    private String snewpw;
+    private String snewpwagain;
+    private boolean isValidInput;
+    private FirebaseAuth mAuth;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_change_password, null);
 
+        currentpw = (TextInputEditText) view.findViewById(R.id.change_un_input);
+        newpw = (TextInputEditText) view.findViewById(R.id.change_newpw_input);
+        newpwagain = (TextInputEditText) view.findViewById(R.id.change_newpw_input1);
+
+        savepw =(Button)view.findViewById(R.id.bt_pw_save) ;
+
+        savepw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkInputIsValid();
+            }
+        });
+        return view;
+    }
+
+    private void checkInputIsValid() {
+        scurrentpw = currentpw.getText().toString().trim();
+        snewpw = newpw.getText().toString().trim();
+        snewpwagain = newpwagain.getText().toString().trim();
+        isValidInput = true;
+        if (scurrentpw.isEmpty() ){
+            currentpw.requestFocus();
+            isValidInput = false;
+            Toast.makeText(getActivity(), getString(R.string.input_password_error), Toast.LENGTH_LONG).show();
+        }
+        if (!snewpw.equals(snewpwagain)) {
+            newpwagain.requestFocus();
+            isValidInput = false;
+            Toast.makeText(getActivity(), getString(R.string.input_password_not_consistent), Toast.LENGTH_LONG).show();
+        }
+        if (isValidInput) {
+            updatePassword();
+        }
+    }
+
+    private void updatePassword() {
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        final String email =user.getEmail();
+        AuthCredential credential = EmailAuthProvider.getCredential(email,scurrentpw);
+
+        user.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    user.updatePassword(snewpw).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(getActivity(),
+                                        "Password has been updated",
+                                        Toast.LENGTH_SHORT).show();
+                            } else {
+                                Log.e(TAG, "Error in updating password",
+                                        task.getException());
+                                Toast.makeText(getActivity(),
+                                        "Failed to update password.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+                else {
+                    Toast.makeText(getActivity(),"Authentication failed",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
         ImageButton goback = (ImageButton)view.findViewById(R.id.btn_pw_reset_back);
         goback.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                FragmentTransaction transaction = getFragmentManager().beginTransaction();
-                SettingFragment fragment = new SettingFragment();
-                transaction.replace(R.id.main_frame,fragment);
-                transaction.addToBackStack(null);
-                transaction.commit();
+                getActivity().onBackPressed();
             }
-        });
+        });}
 
 
 
-        return view;
     }
-
-
-}
--- app/src/main/java/com/example/android/blindchat/ui/PasswordResetActivity.java
+++ app/src/main/java/com/example/android/blindchat/ui/PasswordResetActivity.java
@@ -2,22 +2,29 @@ package com.example.android.blindchat.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.blindchat.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class PasswordResetActivity extends AppCompatActivity {
 
-    ImageButton pwResetBack;
-    TextInputEditText pwResetUsername;
-    TextInputEditText pwResetEmail;
-    Button sendEmail;
-
+    private String TAG = "PasswordResetActivity";
+    private ImageButton pwResetBack;
+    private TextInputEditText pwResetUsername;
+    private TextInputEditText pwResetEmail;
+    private Button sendEmail;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,18 +43,30 @@ public class PasswordResetActivity extends AppCompatActivity {
         pwResetUsername = (TextInputEditText)findViewById(R.id.pw_reset_username);
         pwResetEmail = (TextInputEditText)findViewById(R.id.pw_reset_email);
 
+
         sendEmail = (Button)findViewById(R.id.btn_send_email);
         sendEmail.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                toInit();
+                resetUserPassword();
             }
         });
-
     }
 
-    private void toInit() {
-        Intent intent = new Intent(this, InitActivity.class);
-        startActivity(intent);
+    private void resetUserPassword() {
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.sendPasswordResetEmail(pwResetEmail.getText().toString())
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(getApplicationContext(), "Instructions sent",
+                                    Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(getApplicationContext(),
+                                    "Email don't exist", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
