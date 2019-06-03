afbb7e1d7e962428783ca7db27778952e71b031c
tianxinzhou <zhoutianxin@gmail.com>
2019-06-02 21:56:29 -0700

    joined room list item can be clicked now, the room info button in the list can be clicked now

--- app/src/main/java/com/example/android/blindchat/ui/JoinedRoomFragment.java
+++ app/src/main/java/com/example/android/blindchat/ui/JoinedRoomFragment.java
@@ -3,6 +3,7 @@ package com.example.android.blindchat.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,6 +23,8 @@ import java.util.List;
 
 public class JoinedRoomFragment extends Fragment {
 
+    private static final String TAG = "debugging joinedroom";
+
     private List<JoinedRoom> list = new ArrayList<>();
 
 
@@ -29,23 +32,38 @@ public class JoinedRoomFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_joined, null);
 
+
         list = loadList();
 
+
         JoinedRoomAdapter adapter = new JoinedRoomAdapter(view.getContext(), R.layout.joined_list_item, list);
+
+
         ListView listView = (ListView)view.findViewById(R.id.joined_room_listview);
+
+
         listView.setAdapter(adapter);
+
+
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
+                Log.d(TAG,"si1" );
+
                 //target chatroom
                 JoinedRoom selected = list.get(position);
 
+
+                Log.d(TAG,"si2" );
+
                 //should go to target chatroom
-                Intent intent = new Intent(view.getContext(), Chatroom.class);
-                startActivity(intent);
+                Intent intent = new Intent(view.getContext(), ChatroomActivity.class);
+                view.getContext().startActivity(intent);
             }
         });
+
+
         return view;
     }
 
@@ -53,17 +71,26 @@ public class JoinedRoomFragment extends Fragment {
     //for testing layout purpose, chatroom database needed
     private List<JoinedRoom> loadList(){
 
+
+
         List<Chatroom> cList = new ArrayList<>();
         List<JoinedRoom> jList = new ArrayList<>();
+
+
         for (int i = 0; i < 100; i++) {
+
             Chatroom cRoom = new Chatroom(getResources().getString(R.string.joined_room_title) + " " + i, "Topic: " + i, "");
+
+
             cList.add(cRoom);
         }
         for (int i = 0; i < 100; i++) {
+
             Time temp = new Time(i);
             JoinedRoom jRoom = new JoinedRoom(cList.get(i), temp);
             jList.add(jRoom);
         }
+
 
         return jList;
 
