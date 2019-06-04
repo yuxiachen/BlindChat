51ae17f06ac5fd521a134e33a55542390d3141e4
Celia Liang <celialiang0627@gmail.com>
2019-06-03 21:39:56 -0700

    Don't pass Chatroom object around. Key alone is sufficient

--- app/src/main/java/com/example/android/blindchat/ui/JoinedRoomFragment.java
+++ app/src/main/java/com/example/android/blindchat/ui/JoinedRoomFragment.java
@@ -74,15 +74,14 @@ public class JoinedRoomFragment extends Fragment implements ChatroomAdapter.OnRo
     };
 
 
-    private void openChatroomActivity(Chatroom chatroom, String key) {
+    private void openChatroomActivity(String key) {
         Intent intent = new Intent(getActivity(), ChatroomActivity.class);
-        intent.putExtra("chatroom", chatroom);
         intent.putExtra("key", key);
         startActivity(intent);
     }
 
     @Override
-    public void OnRoomItemClicked(Chatroom chatroom, String key) {
-        openChatroomActivity(chatroom, key);
+    public void OnRoomItemClicked(String key) {
+        openChatroomActivity(key);
     }
 }
